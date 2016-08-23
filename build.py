37a87e36c50de3b210048395db433c03b0e90623
Robert Cohn <robert.s.cohn@intel.com>
2016-08-22 20:28:34 -0500

    set docker repo name

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import subprocess
 
-repo = 'rscohn2/bench.ubuntu.docker'
+repo = 'rscohn2/bench.ubuntu.python'
 subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
 subprocess.check_call('docker build -t %s .' % repo, shell=True)
 subprocess.check_call('docker push %s' % repo,shell=True)
