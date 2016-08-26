614b3b3fbcc710182afe8a070312ff4442766a19
Robert Cohn <robert.s.cohn@intel.com>
2016-08-26 10:58:21 -0500

    refactor for apt/pip and blas variants

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
 import subprocess
 
+def build(ext, push=True):
+    repo = 'rscohn2/bench.ubuntu.%s' % ext
+    subprocess.check_call('docker build -t %s -f Dockerfile.%s .' % (repo,ext), shell=True)
+    if push:
+        subprocess.check_call('docker push %s' % repo,shell=True)
+
+
 subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
-for version in ['pip','apt']:
-    repo = 'rscohn2/bench.ubuntu.%s.python' % version
-    subprocess.check_call('docker build -t %s -f Dockerfile.%s .' % (repo,version), shell=True)
-    subprocess.check_call('docker push %s' % repo,shell=True)
+build('base', False)
+build('pip')
+build('apt.openblas')
+build('apt.reference')
+build('apt.atlas')
