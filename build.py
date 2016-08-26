a86f99114edd04b85060ebe3e1756a9b7bf523d6
Robert Cohn <robert.s.cohn@intel.com>
2016-08-25 21:52:02 -0500

    apt vs pip

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import subprocess
 
-#subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
-for version in ['14.04','16.04']:
-    repo = 'rscohn2/bench.ubuntu%s.python' % version
-    subprocess.check_call('docker build -t %s -f Dockerfile.%s .' % (version,version), shell=True)
-    #subprocess.check_call('docker push %s' % repo,shell=True)
+subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
+for version in ['pip','apt']:
+    repo = 'rscohn2/bench.ubuntu.%s.python' % version
+    subprocess.check_call('docker build -t %s -f Dockerfile.%s .' % (repo,version), shell=True)
+    subprocess.check_call('docker push %s' % repo,shell=True)
