9f8fc7f058463f77e92e36f33f15cbd03616431e
Robert Cohn <robert.s.cohn@intel.com>
2016-08-28 11:46:03 -0500

    configure lapack; some support for centos, but not working yet

--- build.py
+++ build.py
@@ -1,15 +1,23 @@
+import os
 import subprocess
 
+def get_proxies():
+    proxies = ''
+    for var in ['http_proxy','https_proxy','no_proxy']:
+        if var in os.environ:
+            proxies += ' --build-arg %s=%s' % (var,os.environ[var])
+    return proxies
+
 def build(ext, push=True):
-    repo = 'rscohn2/bench.ubuntu.%s' % ext
-    subprocess.check_call('docker build -t %s -f Dockerfile.%s .' % (repo,ext), shell=True)
+    repo = 'rscohn2/bench.%s' % ext
+    subprocess.check_call('docker build %s -t %s -f Dockerfile.%s .' % (get_proxies(),repo,ext), shell=True)
     if push:
         subprocess.check_call('docker push %s' % repo,shell=True)
 
 
 subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
-build('base', False)
-build('pip')
-build('apt.openblas')
-build('apt.reference')
-build('apt.atlas')
+build('ubuntu.base', False)
+#build('centos.base', False)
+for v in ['ubuntu.pip','apt.openblas','apt.reference','apt.atlas']:
+    build(v,False)
+
